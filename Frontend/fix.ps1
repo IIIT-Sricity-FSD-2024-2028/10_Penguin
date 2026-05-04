182dce1df7433edb53c7d6b8236a57d0ebba4e36
saiganesh1586 <saiganesh.c24@iiits.in>
2026-05-05 02:42:16 +0530

    Delete Frontend directory

--- Frontend/fix.ps1
+++
@@ -1,5 +0,0 @@
-$file = 'c:\Users\panth\Downloads\FFSD_project\FFSD_project\Index.html'
-$content = Get-Content -Path $file -Raw
-$fixed = $content -replace '₹', '$'
-Set-Content -Path $file -Value $fixed -Encoding UTF8
-Write-Host "File fixed!"
